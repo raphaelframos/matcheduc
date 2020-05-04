ee69b56274e04d120cdaca20c906b20a333e82ed
Raphael Ramos <raphaelframos@gmail.com>
2020-05-03 21:23:06 -0300

    Listener do card

--- app/src/main/java/powellapps/matcheduc/ui/cardstack/TinderFragment.kt
+++ app/src/main/java/powellapps/matcheduc/ui/cardstack/TinderFragment.kt
@@ -1,14 +1,13 @@
 package powellapps.matcheduc.ui.cardstack
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.yuyakaido.android.cardstackview.CardStackLayoutManager
-import com.yuyakaido.android.cardstackview.Direction
-import com.yuyakaido.android.cardstackview.StackFrom
-import com.yuyakaido.android.cardstackview.SwipeableMethod
+import android.widget.Toast
+import com.yuyakaido.android.cardstackview.*
 
 import powellapps.matcheduc.R
 import powellapps.matcheduc.databinding.FragmentTinderBinding
@@ -17,27 +16,61 @@ import powellapps.matcheduc.repository.ThemeRepository
 /**
  * A simple [Fragment] subclass.
  */
-class TinderFragment : Fragment() {
+class TinderFragment : Fragment(), CardStackListener {
 
-    val manager by lazy { CardStackLayoutManager(context) }
+    val manager by lazy { CardStackLayoutManager(context, this) }
+    val adapter by lazy { CardStackThemeAdapter(ThemeRepository.create()) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         var binding = FragmentTinderBinding.inflate(inflater, container, false)
-
         initializeLayoutManager()
         binding.cardStackTheme.layoutManager = manager
         binding.cardStackTheme.adapter = CardStackThemeAdapter(ThemeRepository.create())
+        binding.floatingActionButtonRewind.setOnClickListener {
+       //     adapter.themes = ThemeRepository.create()
+         //   adapter.notifyDataSetChanged()
+            binding.cardStackTheme.rewind()
+
+        }
+
         return binding.root
     }
+
+
 
     private fun initializeLayoutManager() {
         manager.setVisibleCount(3)
         manager.setStackFrom(StackFrom.None)
         manager.setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
         manager.setDirections(Direction.HORIZONTAL)
+    }
+
+    override fun onCardDisappeared(view: View?, position: Int) {
+        Log.d("CardStackView", "onCardCanceled: ${manager.topPosition}")
+    }
+
+    override fun onCardDragging(direction: Direction?, ratio: Float) {
+        Log.d("CardStackView", "onCardCanceled: ${manager.topPosition}")
+
+    }
+
+    override fun onCardSwiped(direction: Direction?) {
+        Toast.makeText(context, "Teste " + direction!!.ordinal, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onCardCanceled() {
+        Log.d("CardStackView", "onCardCanceled: ${manager.topPosition}")
+    }
+
+    override fun onCardAppeared(view: View?, position: Int) {
+        Log.d("CardStackView", "onCardCanceled: ${manager.topPosition}")
+    }
+
+    override fun onCardRewound() {
+        Log.d("CardStackView", "onCardCanceled: ${manager.topPosition}")
     }
 
 
