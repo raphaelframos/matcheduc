22b4715b96484c9e176e14a744fa2040ba9fbacd
Raphael Ramos <raphaelframos@gmail.com>
2020-05-05 17:12:06 -0300

    Trocando tema do app

--- app/src/main/java/powellapps/matcheduc/repository/ThemeRepository.kt
+++ app/src/main/java/powellapps/matcheduc/repository/ThemeRepository.kt
@@ -7,7 +7,8 @@ class ThemeRepository {
     companion object {
         fun create(): List<Theme> {
             val themes = ArrayList<Theme>()
-            themes.add(Theme("Natureza", "Darwin\n\nConhecendo as bacterias\n\nIabadabaduIabadabaduIabadabaduIabadabaduIabadabadu\n\nNatureza 4Natureza 4Natureza 4Natureza 4Natureza 4"))
+            themes.add(Theme("O Bullying nas redes sociais", "O texto argumentativo\n\nElementos da argumentação" +
+                    "\n\nVozes na argumentação\n\nEstratégias argumentativas"))
             themes.add(Theme("NaturezaNaturezaNaturezaNaturezaNaturezaNaturezaNatureza", "Darwin\n\nConhecendo as bacterias\n\nIabadabaduIabadabaduIabadabaduIabadabaduIabadabadu\n\nNatureza 4Natureza 4Natureza 4Natureza 4Natureza 4"))
 
             return themes
